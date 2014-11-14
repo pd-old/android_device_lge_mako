bef68f8d621e661cb1f54307678b6dc81a4c4649
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-11-15 00:17:56 +0200

    call disco vendor
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>

--- full_mako.mk
+++ full_mako.mk
@@ -37,6 +37,10 @@ PRODUCT_MODEL := AOSP on Mako
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/mako/device.mk)
 $(call inherit-product-if-exists, vendor/lge/mako/device-vendor.mk)
+
+# call disco vendor
+$(call inherit-product-if-exists, vendor/disco/main.mk)
